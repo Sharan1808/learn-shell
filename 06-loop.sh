4f6ef5b4ca1f016dfe5ba9e68d506baf851ca7d6
Sharan <sharanpyata1808@gmail.com>
2023-11-07 02:10:53 -0600

    Adding shell topics

---
+++ 06-loop.sh
@@ -0,0 +1,19 @@
+#While and For loop
+
+#While loop for expressions
+#For loop is if you having inputs
+
+a=10
+while [ "$a" -gt 0 ]; do
+  echo Hello World
+  a=$(($a-1))
+  sleep 1
+done
+
+for fruit in apple banana orange; do
+  eho Fruit Name -$fruit
+  sleep 1
+done
+
+
+
